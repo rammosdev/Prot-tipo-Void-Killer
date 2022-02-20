1100774dc730c38f5a1a2728b3a958a4f4525867
Bladedragonx <ticksobrinho@hotmail.com>
2022-02-20 19:23:33 -0300

    Update: Atualizando o Tiro Automatico para buscar o inimigo mais proximo

--- Void Hunter - Protótipos/Assets/Scripts/Shooting.cs
+++ Void Hunter - Protótipos/Assets/Scripts/Shooting.cs
@@ -9,6 +9,7 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
 
     public Transform enemy;
+    public GameObject[] Enemies;
 
     float timeBetween;
     public float startTimeBetween;
@@ -21,10 +22,11 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject EnemyNearDistance = GetEnemyNearby();
         if (timeBetween <= 0)
         {
-            Vector3 targetDirection = transform.position - enemy.position;
+            Vector3 targetDirection = transform.position - EnemyNearDistance.transform.position ;
             Instantiate(bullet, firePoint.position, Quaternion.LookRotation(new Vector3(0, 0, 1), -targetDirection));
             timeBetween = startTimeBetween;
         }
@@ -32,6 +34,23 @@ public class Shooting : MonoBehaviour
         {
             timeBetween -= Time.deltaTime;
         }
+    }
+
+    private GameObject GetEnemyNearby() {
+
+        var NearDistance = float.MaxValue;
+        GameObject EnemyNearDistance = null;
+
+        foreach(var Enemy in Enemies)
+        {
+            if(Vector3.Distance(Enemy.transform.position, GameObject.Find("Char").transform.position) < NearDistance)
+            {
+                NearDistance = Vector3.Distance(Enemy.transform.position, GameObject.Find("Char").transform.position);
+                EnemyNearDistance = Enemy;
+            }
+        }
+        //Debug.DrawLine(GameObject.Find("Char").transform.position, EnemyNearDistance.transform.position, Color.red);
+        return EnemyNearDistance;
     }
 
 }
